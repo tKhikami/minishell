c58cd4efcf292da4cbd0eadbd6dcd4ffe124aebd
RANDRIANIRINA Ny Toky Antonio <tokyrakotomahefa@gmail.com>
2024-09-03 13:15:09 +0300

    makefile added

---
+++ main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyrandri <nyrandri@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 08:43:58 by nyrandri          #+#    #+#             */
+/*   Updated: 2024/09/03 12:30:18 by nyrandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	readline("test");
+	return (0);
+}
