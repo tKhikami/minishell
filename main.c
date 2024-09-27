fe43f8725e1fff97e9aff899147ee2ff96290865
RANDRIANIRINA Ny Toky Antonio <tokyrakotomahefa@gmail.com>
2024-09-27 12:00:22 +0300

    lambda change

--- main.c
+++ main.c
@@ -34,12 +34,6 @@ void	ft_get_line(int *exit_status)
 			rl_clear_history();
 			ft_printf("exit\n");
 			free(str);
-			exit(EXIT_SUCCESS);
-		}
-	}
-	if (str)
-		free(str);
-	exit(EXIT_SUCCESS);
 			return ;
 		}
 	}
@@ -52,8 +46,7 @@ int	main(int n, char *arg[], char *env[])
 
 	exit_status = 0;
 	(void)arg;
-	//(void)env;
-	ft_export(env);
+	(void)env;
 	if (n == 1)
 	{
 		signal(SIGINT, ft_handle_signals);
